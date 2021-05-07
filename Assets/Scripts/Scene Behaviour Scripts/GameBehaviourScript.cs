53068a477c8f113d1d645cd5b0ffca548c5cdc3b
Daniel Benedí García <774156@unizar.es>
2021-05-07 13:37:23 +0200

    Introducido nuevo TAD para gestion de usuarios
    Además se ha cambiado useDice porque usaba el rango [2,7) y se quería [1,7).

--- Assets/Scripts/Scene Behaviour Scripts/GameBehaviourScript.cs
+++ Assets/Scripts/Scene Behaviour Scripts/GameBehaviourScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -15,54 +16,64 @@ public class GameBehaviourScript : MonoBehaviour
     public GameObject Player5;
     public GameObject Player6;
 
+    private GameObject[] Players;
+
     private Button[] boardFields;
     private int diceNumber;
     //Images are [Avatar,Token,Frame,qP,qO,qB,qG,qP,qY]
-    private Text nameP1;
-    private Image[] imgP1;
-    private Text nameP2;
-    private Image[] imgP2;
-    private Text nameP3;
-    private Image[] imgP3;
-    private Text nameP4;
-    private Image[] imgP4;
-    private Text nameP5;
-    private Image[] imgP5;
-    private Text nameP6;
-    private Image[] imgP6;
+    /*
+        Purple  -> Cultura General
+        Orange  -> Deportes
+        Blue    -> Geografía
+        Green   -> Ciencias
+        Pink    -> Entretenimiento
+        Yellow  -> Historia
+
+     */
+    private readonly string[] categories = { "Cultura General", "Deportes", "Geografia", "Ciencias", "Entretenimiento", "Historia" };
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Players = new GameObject[] { Player1, Player2, Player3, Player4, Player5, Player6 };
+        if (SocketioHandler.op.Equals("reconexion"))
+        {
+            startReconnection();
+        }
         setPlayersAtStart();
     }
 
+
+    public readonly static Queue<Action> ExecuteOnMainThread = new Queue<Action>();
     // Update is called once per frame
     void Update()
     {
-        
+        while (ExecuteOnMainThread.Count > 0)
+        {
+            ExecuteOnMainThread.Dequeue().Invoke();
+        }
+
     }
 
     private void setPlayersAtStart()
     {
-        nameP1 = Player1.GetComponentInChildren<Text>();
-        imgP1 = Player1.GetComponentsInChildren<Image>(true);
-        nameP2 = Player2.GetComponentInChildren<Text>();
-        imgP2 = Player2.GetComponentsInChildren<Image>(true);
-        nameP3 = Player3.GetComponentInChildren<Text>();
-        imgP3 = Player3.GetComponentsInChildren<Image>(true);
-        nameP4 = Player4.GetComponentInChildren<Text>();
-        imgP4 = Player4.GetComponentsInChildren<Image>(true);
-        nameP5 = Player5.GetComponentInChildren<Text>();
-        imgP5 = Player5.GetComponentsInChildren<Image>(true);
-        nameP6 = Player6.GetComponentInChildren<Text>();
-        imgP6 = Player6.GetComponentsInChildren<Image>(true);
+        for(int i = 0; i < 6; i++)
+        {
+            Players[i].SetActive(i < PlayersDataScript.jugadores.Count);
+            if(i < PlayersDataScript.jugadores.Count)
+            {
+                Players[i].GetComponentInChildren<Text>().text = PlayersDataScript.jugadores[i].nombre;
+                Players[i].GetComponentsInChildren<Image>(true)[0].sprite = Resources.Load<Sprite>("Avatar/" + PlayersDataScript.jugadores[i].avatar);
+                Players[i].GetComponentsInChildren<Image>(true)[1].sprite = Resources.Load<Sprite>("Token/" + PlayersDataScript.jugadores[i].ficha);
+                foreach(string img in PlayersDataScript.jugadores[i].quesitos)
+                {
+                    int idx = Array.FindIndex(categories, (c) => { return c.Equals(img); });
+                    Players[i].GetComponentsInChildren<Image>(true)[idx].gameObject.SetActive(true);
+                }
+            }
+        }
 
-        //Test modificación jugador. Sustituir.
-        nameP1.text = "TestPlayer1";
-        imgP1[0].sprite = Resources.Load<Sprite>("Avatar/avatar1");
-        imgP1[1].sprite = Resources.Load<Sprite>("Token/ficha1");
-        imgP1[5].gameObject.SetActive(true);
     }
 
     private void playTurn(int player)
@@ -73,7 +84,7 @@ public class GameBehaviourScript : MonoBehaviour
 
     private void useDice()
     {
-        diceNumber = Random.Range(2, 7);    //(minInclusive..maxExclusive)
+        diceNumber = UnityEngine.Random.Range(1, 7);    //(minInclusive..maxExclusive)
     }
 
     private void newQuestion(string category)
@@ -82,5 +93,10 @@ public class GameBehaviourScript : MonoBehaviour
         QuestionDataScript.setQuestion("¿1+1?","1","2","3","4",2);
 
         SceneManager.LoadScene("Question Scene", LoadSceneMode.Additive);
+    }
+
+    private void startReconnection()
+    {
+
     }
 }
